4b25ab419df79ae7b3e7680b58ac2320f5e7fc24
Neo Esil <burningteamhihi@gmail.com>
2016-12-22 07:55:51 +0700

    publish react

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path=require('path');
+var webpack=require('webpack');
+
+module.exports={
+	entry:__dirname+'/app/index.js',
+	output:{
+		path:__dirname+'/public/',
+		filename:'bundle.js'
+	},
+	module:{
+		loaders:[
+			{
+				test:/.jsx?$/,
+				loader:'babel-loader',
+				include:path.join(__dirname,'app'),
+				exclude:/(node_modules)/,
+				query:{
+					presets:['es2015','react','stage-0']
+				}
+			}
+		]
+	}
+}
+
